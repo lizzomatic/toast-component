0ae8b9933a79a6f57dc67d733c074001b0fa9d54
lizzomatic <lizzomatic@gmail.com>
2024-01-27 21:35:08 -0800

    added some files

---
+++ src/components/ToastShelf/ToastShelf.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { ToastContext } from '../ToastProvider';
+import Toast from '../Toast';
+import styles from './ToastShelf.module.css';
+
+function ToastShelf() {
+  const { toasts } = React.useContext(ToastContext);
+  return (
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notifications"
+    >
+
+      {toasts.map((toast) => (
+        <li key={toast.id} className={styles.toastWrapper}>
+          <Toast
+            id={toast.id}
+            variant={toast.variant}
+          >
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default ToastShelf;
